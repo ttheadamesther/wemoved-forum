c7799c570454ebd70213ac1dbbcb05bc249531ab
theadamesther <adam.esther@outlook.com>
2026-06-02 18:15:14 +0200

    fix: agrandissement logo navbar

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import { LOGO_B64 } from '../lib/constants'
 
-export const Logo = ({ height = 48 }) => (
+export const Logo = ({ height = 80 }) => (
   <img
     src={`data:image/png;base64,${LOGO_B64}`}
     alt="wemoved"
