d2139a0f6fe86d91d4a07575619f2128dfbb74b1
theadamesther <adam.esther@outlook.com>
2026-06-03 16:41:37 +0200

    fix: logo deux versions light/dark - texte noir en mode clair

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,20 +1,13 @@
-import { LOGO_B64 } from '../lib/constants'
+import { LOGO_B64, LOGO_B64_LIGHT } from '../lib/constants'
 import { useTheme } from '../hooks/ThemeContext'
 
 export const Logo = ({ height = 200 }) => {
   const { dark } = useTheme()
   return (
     <img
-      src={`data:image/png;base64,${LOGO_B64}`}
+      src={`data:image/png;base64,${dark ? LOGO_B64 : LOGO_B64_LIGHT}`}
       alt="wemoved"
-      style={{
-        height,
-        display: 'block',
-        cursor: 'pointer',
-        objectFit: 'contain',
-        imageRendering: 'auto',
-        mixBlendMode: dark ? 'normal' : 'multiply',
-      }}
+      style={{ height, display: 'block', cursor: 'pointer', objectFit: 'contain', imageRendering: 'auto' }}
     />
   )
 }
