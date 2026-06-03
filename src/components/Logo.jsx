8fb0a3934ad57a1c9390a4be3c364044c701b040
theadamesther <adam.esther@outlook.com>
2026-06-03 16:44:32 +0200

    fix: logo fond extérieur supprimé, texture intérieure conservée

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,9 @@
-import { LOGO_B64, LOGO_B64_LIGHT } from '../lib/constants'
-import { useTheme } from '../hooks/ThemeContext'
+import { LOGO_B64 } from '../lib/constants'
 
-export const Logo = ({ height = 200 }) => {
-  const { dark } = useTheme()
-  return (
-    <img
-      src={`data:image/png;base64,${dark ? LOGO_B64 : LOGO_B64_LIGHT}`}
-      alt="wemoved"
-      style={{ height, display: 'block', cursor: 'pointer', objectFit: 'contain', imageRendering: 'auto' }}
-    />
-  )
-}
+export const Logo = ({ height = 200 }) => (
+  <img
+    src={`data:image/png;base64,${LOGO_B64}`}
+    alt="wemoved"
+    style={{ height, display: 'block', cursor: 'pointer', objectFit: 'contain', imageRendering: 'auto' }}
+  />
+)
